be57479f969d80b0b4e6a690d6a05513160443ef
ministeren <fcmikkelsen@gmail.com>
2015-03-14 17:40:06 +0100

    Add: klassestruktur

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Spiller spiller1 = new Spiller("Borg", true);
+		Spiller spiller2 = new Spiller("McEnroe", false);
+		
+		spiller1.SetModstander(spiller2);
+		spiller2.SetModstander(spiller1);
+		
+		Thread spiller1Thread = new Thread((Runnable) spiller1);
+		Thread spiller2Thread = new Thread((Runnable) spiller2);
+		
+		spiller1Thread.start();
+		spiller2Thread.start();
+		
 	}
 
 }
--- src/Spiller.java
+++ src/Spiller.java
@@ -1,4 +1,27 @@
 
-public class Spiller {
+public class Spiller implements Runnable {
 
+	private Spiller modstander;
+	private String name;
+	private boolean ball=false;
+	
+	public Spiller(String name, boolean ball){
+		this.name=name;
+		this.ball=ball;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void SetModstander(Spiller modstander){
+		this.modstander=modstander;
+	}
+	
+	public void modtagBold(){
+		this.ball=true;
+	}
+	
 }
