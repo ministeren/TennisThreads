50e968ea9e4b4f12f62ebb30fda608e38220d2b4
ministeren <fcmikkelsen@gmail.com>
2015-03-14 18:39:58 +0100

    Add: funktionelt program

--- src/Spiller.java
+++ src/Spiller.java
@@ -18,12 +18,18 @@ public class Spiller implements Runnable {
 		// TODO Auto-generated method stub
 		while(alive){
 			if(ball && modstander.isAlive()){
+				System.out.println(name+" forsøger et slag");
 				Sleeper.sleepRandom(2);
 				Random r = new Random();
 				int hit = r.nextInt(100)+1;
 				if(hit <= 80){
+					System.out.println("og rammer!");
+					System.out.println();
 					modstander.modtagBold();
+					ball=false;
 				} else {
+					System.out.println("og misser!");
+					System.out.println();
 					alive=false;
 				}
 
